256255fd85703518877cf0da39654961a3cdeb84
shashwat srivastava <113302101+SATYAM31104@users.noreply.github.com>
2025-10-31 15:37:36 +0530

    commit 3

--- ConsoleProject/Program.js
+++ ConsoleProject/Program.js
@@ -1,6 +1,8 @@
-const arr = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArr = [...new Set(arr)];
-console.log("Unique Array:", uniqueArr);
 
-const doubledArr = uniqueArr.map(x => x * 2);
-console.log("Doubled Array:", doubledArr);
+const removeDuplicates = (array) => {
+    return [...new Set(array)];
+};
+
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+const uniqueNumbers = removeDuplicates(numbers);
+console.log(uniqueNumbers); 
